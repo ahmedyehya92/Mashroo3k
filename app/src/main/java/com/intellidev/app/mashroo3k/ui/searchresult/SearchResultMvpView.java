6938aa81ab2d6a35782b0942b4ccff0d0a59e33c
ahmedyehya92 <ahmedyehya1992@gmail.com>
2018-04-06 03:04:24 +0200

    test

---
+++ app/src/main/java/com/intellidev/app/mashroo3k/ui/searchresult/SearchResultMvpView.java
@@ -0,0 +1,10 @@
+package com.intellidev.app.mashroo3k.ui.searchresult;
+
+import com.intellidev.app.mashroo3k.ui.base.MvpView;
+
+/**
+ * Created by ahmedyehya on 4/6/18.
+ */
+
+public interface SearchResultMvpView extends MvpView {
+}
